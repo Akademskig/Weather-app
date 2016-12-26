3b059f79e9cf353902e0366133db77860eebd446
Akademskig <m.susek@live.com>
2016-12-26 15:53:00 +0100

    Made it prettier

--- script.js
+++ script.js
@@ -1,47 +1,45 @@
- $.getJSON("http://ip-api.com/json", function(jsonPos){  
-      var lat = jsonPos.lat;
-      var lon = jsonPos.lon;
-     
-      var call = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=b8f4759ff5ad0b84e904462161d6bb60";
-      
-      $.get(call + "&units=metric", "jsonp")
-        .done(function(jsonData) {
-          var weatherData = {};
-          weatherData = jsonData;
-          var tempC = weatherData.main.temp.toFixed(1);
-          var weather = weatherData.weather[0].main;
-          var humidity = weatherData.main.humidity;
-          var pressure = weatherData.main.pressure;
-          var windKph = weatherData.wind.speed;
-          var location = jsonPos.city + ", " + weatherData.sys.country;
-          var iconid = weatherData.weather[0].id;
-          var winddeg = weatherData.wind.deg;
-        
-          $(".btn").html(tempC + "° C");
-          $("#weather").html("Weather: " + weather);
-          $("#humidity").html("Humidity: " + humidity + "%");
-          $("#pressure").html("Pressure: " + pressure + " hPa");
-          $(".wind").html("Wind Speed: " + windKph + " m/s");
-          $("#loc").html(location);
-          $("#icon").html("<i class=" + "\"wi wi-owm-" + iconid + "\"></i>");
-        
-          var tempF = (tempC * 9 / 5 + 32).toFixed(1);
-          var windmph = (windKph * 0.621371192).toFixed(1);
-         
-          $(".btn").on("click", function() {
-            if ($(".btn").is(":contains('° C')")) {
-              $(".btn").html(tempF + "° F");
-              $(".wind").html("Wind Speed: " + windmph + " mph");
-              $(".btn").console.log;
-            }
-            if ($(".btn").is(":contains('° F')")) {
-              $(".btn").html(tempC + "° C");
-              $(".wind").html("Wind Speed: " + windKph + " kph");
-            }
-          });
-        })
-        .error(function(error) {
-          console.log(error)
-        });
-    })
+$.getJSON("http://ip-api.com/json", function(jsonPos) {
+  var lat = jsonPos.lat;
+  var lon = jsonPos.lon;
 
+  var call = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=b8f4759ff5ad0b84e904462161d6bb60";
+
+  $.get(call + "&units=metric", "jsonp")
+    .done(function(jsonData) {
+      var weatherData = {};
+      weatherData = jsonData;
+      var tempC = weatherData.main.temp.toFixed(1);
+      var weather = weatherData.weather[0].main;
+      var humidity = weatherData.main.humidity;
+      var pressure = weatherData.main.pressure;
+      var windKph = weatherData.wind.speed;
+      var location = jsonPos.city + ", " + weatherData.sys.country;
+      var iconid = weatherData.weather[0].id;
+
+      $(".btn").html(tempC + "° C");
+      $("#weather").html("Weather: " + weather);
+      $("#humidity").html("Humidity: " + humidity + "%");
+      $("#pressure").html("Pressure: " + pressure + " hPa");
+      $(".wind").html("Wind Speed: " + windKph + " m/s");
+      $("#loc").html(location);
+      $("#icon").html("<i class=" + "\"wi wi-owm-" + iconid + "\"></i>");
+
+      var tempF = (tempC * 9 / 5 + 32).toFixed(1);
+      var windmph = (windKph * 0.621371192).toFixed(1);
+
+      $(".btn").on("click", function() {
+        if ($(".btn").is(":contains('° C')")) {
+          $(".btn").html(tempF + "° F");
+          $(".wind").html("Wind Speed: " + windmph + " mph");
+          $(".btn").console.log;
+        }
+        if ($(".btn").is(":contains('° F')")) {
+          $(".btn").html(tempC + "° C");
+          $(".wind").html("Wind Speed: " + windKph + " kph");
+        }
+      });
+    })
+    .error(function(error) {
+      console.log(error)
+    });
+})
